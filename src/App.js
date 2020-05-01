25e4d84d6283daa1caa3da76912ddd280e36dd32
unknown <tom1130@naver.com>
2020-05-01 22:59:28 +0900

    'modify'

--- src/App.js
+++ src/App.js
@@ -37,7 +37,8 @@ class App extends React.Component{
                   year={movie.year}
                   title={movie.title}
                   summary={movie.summary}
-                  poster={movie.medium_cover_image} />
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres} />
               ))}
             </div>
           ) 
