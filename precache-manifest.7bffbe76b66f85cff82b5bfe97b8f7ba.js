5536d46ff17e996ae33125746a241376533dc0e8
unknown <tom1130@naver.com>
2020-05-01 20:39:08 +0900

    Updates

---
+++ precache-manifest.7bffbe76b66f85cff82b5bfe97b8f7ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4b751be4e31a1b66dc9b2669e1509d",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "ed731474cb35ca3ebb5c",
+    "url": "/movie_app_react/static/css/main.f8426532.chunk.css"
+  },
+  {
+    "revision": "37bd777fbcf578d1cc67",
+    "url": "/movie_app_react/static/js/2.f394f677.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_react/static/js/2.f394f677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed731474cb35ca3ebb5c",
+    "url": "/movie_app_react/static/js/main.2b44d286.chunk.js"
+  },
+  {
+    "revision": "94c2dfefc4c9ab540a82",
+    "url": "/movie_app_react/static/js/runtime-main.6c95ba5b.js"
+  }
+]);
